50278db67abea2420bad230791ba72801f0b3ce3
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-09-08 17:45:29 -0500

    My first try was moved to complex_predictor_first and my most recent update is in complex_predictor. Got to 1.0 accuracy by updating .each method in good_token_count method.

--- lib/complex_predictor.rb
+++ lib/complex_predictor.rb
@@ -9,44 +9,26 @@ class ComplexPredictor < Predictor
     @data = {}
 
     @all_books.each do |category, books|
-      @data[category] = {
-        words: 0,
-        books: 0,
-        top_words: []
-      }
+      @data[category] = []
       books.each do |filename, tokens|
-        @data[category][:words] += tokens.count
-        @data[category][:books] += 1
-        good_token_count(tokens).each do |x|
-          @data[category][:top_words] << x
-        end
-        title_start = tokens.index("title")+1
-        title_end = tokens.index("author")-1
-        title_words = tokens[title_start..title_end]
-        title_words.each do |x|
-          @data[category][:top_words] << x
+        good_token_count(tokens[300..-300]).each do |x|
+          @data[category] << x
         end
       end
     end
   end
 
-  def good_token_count(tokens, num=20)
-    @good_token_count = {}
+  def good_token_count(tokens, num=70)
     @top_words = []
+    result = tokens.each_with_object(Hash.new(0)) { |word, counts| counts[word] += 1}
 
-    tokens.each do |t|
-      if @good_token_count[t] 
-        @good_token_count[t] += 1
-      else
-        @good_token_count[t] = 1
-      end
+    cutoff_val = result.values.sort[-1 * num]
+    cutoff_words = result.select{|k,v| v >= cutoff_val}
+    cutoff_words.each do |word, count|
+      @top_words << word
     end
 
-    cutoff_val = @good_token_count.values.sort[-1 * num]
-    cutoff_words = @good_token_count.select{|k,v| v>= cutoff_val}
-    cutoff_words.each do |x|
-      @top_words << x
-    end
+    #result.sort_by {|word, counts| counts.reverse}
 
     @top_words
   end
@@ -57,28 +39,14 @@ class ComplexPredictor < Predictor
   #
   # Returns a category.
   def predict(tokens)
-    token_count = tokens.count
-    predict_common_words = []
-
-    good_token_count(tokens).each do |x|
-      predict_common_words << x
-    end
-
-    title_start = tokens.index("title")+1
-    title_end = tokens.index("author")-1
-    title_words = tokens[title_start..title_end]
-    title_words.each do |x|
-      predict_common_words << x
-    end
+    start = tokens.length/4
+    predict_common_words = good_token_count(tokens[start..(start*2)])
 
     predicted_category = nil
     counter = 0
 
     @data.each do |category, cat_data|
-      if title_words.include?(category.to_s)
-        return category
-      end
-      matching_words = (predict_common_words & cat_data[:top_words])
+      matching_words = (predict_common_words & cat_data)
       max_matches = matching_words.size
 
       if max_matches > counter
@@ -91,4 +59,6 @@ class ComplexPredictor < Predictor
 
   end
 end
+
+
 
